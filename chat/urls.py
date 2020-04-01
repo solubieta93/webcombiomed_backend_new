9393d34e8438223d738e34ad86cf1cefa157b504
Solangel <solubieta93@gmail.com>
2020-04-01 14:29:36 -0600

    updating

--- chat/urls.py
+++ chat/urls.py
@@ -3,7 +3,7 @@ from rest_framework import routers
 from django.urls import path
 
 router = routers.DefaultRouter()
-router.register('chat', ChatSessionViewSet, basename='chat')
+router.register('chat', ChatSessionViewSet, base_name='chat')
 # router.register('api/chat/<uri>', ChatSessionViewSet, basename='chat_uri')
 router.register('chat/messages', ChatSessionMessageViewSet, basename='chat_messages')
 
